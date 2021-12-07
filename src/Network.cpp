b182a3845ee05db2f450c6529b65c083735aa760
Mateusz Kojro <mateuszkojro@outlook.com>
2021-12-07 10:18:16 +0100

    Working on fixixng log loss

--- src/Network.cpp
+++ src/Network.cpp
@@ -1,27 +1,25 @@
 //
-// Created by mateusz on 11/22/2021.
+// Created by M.Kojro on 11/22/2021.
 //
+
 #include <Network.h>
 
-Network::Network(size_t input_size,
-                 size_t hidden_size,
-                 size_t output_size,
-                 bool bias,
-                 bool randomize,
+Network::Network(size_t input_size, size_t hidden_size, size_t output_size,
+                 bool bias, bool randomize,
                  std::function<double(double)> activation_function,
                  std::function<double(double)> activation_function_derivative) {
 
   activation_function_ = std::move(activation_function);
   activation_function_derivative_ = std::move(activation_function_derivative);
 
-  hidden_ = Eigen::MatrixXd(hidden_size, 1u);
-  input_ = Eigen::MatrixXd(input_size, 1u);
-  input_bias_ = Eigen::MatrixXd(input_size, 1u);
-  hidden_bias_ = Eigen::MatrixXd(hidden_size, 1u);
-  output_bias_ = Eigen::MatrixXd(output_size, 1u);
+  hidden_ = Eigen::MatrixXd(hidden_size, 1U);
+  input_ = Eigen::MatrixXd(input_size, 1U);
+  input_bias_ = Eigen::MatrixXd(input_size, 1U);
+  hidden_bias_ = Eigen::MatrixXd(hidden_size, 1U);
+  output_bias_ = Eigen::MatrixXd(output_size, 1U);
   input_hidden_weights_ = Eigen::MatrixXd(hidden_size, input_size);
   hidden_output_weights_ = Eigen::MatrixXd(output_size, hidden_size);
-  output_errors_ = Eigen::MatrixXd(output_size, 1u);
+  output_errors_ = Eigen::MatrixXd(output_size, 1U);
 
   if (randomize) {
     hidden_.setRandom();
@@ -45,7 +43,8 @@ Network::Network(size_t input_size,
   }
 }
 
-Eigen::MatrixXd Network::HadamardProduct(const Eigen::MatrixXd &a, const Eigen::MatrixXd &b) {
+Eigen::MatrixXd Network::HadamardProduct(const Eigen::MatrixXd &a,
+                                         const Eigen::MatrixXd &b) {
   return a.cwiseProduct(b);
 }
 
@@ -53,34 +52,40 @@ const Eigen::MatrixXd &Network::FeedForward(const Eigen::MatrixXd &input) {
 
   output_ = input + input_bias_;
   input_ = output_;
-  output_.unaryExpr(activation_function_);
+  output_ = output_.unaryExpr(activation_function_);
   activation_input_ = output_;
 
   output_ = input_hidden_weights_ * output_;
   output_ += hidden_bias_;
   hidden_ = output_;
-  output_.unaryExpr(activation_function_);
+  output_ = output_.unaryExpr(activation_function_);
   activation_hidden_ = output_;
 
   output_ = hidden_output_weights_ * output_;
   output_ += output_bias_;
   activation_output_ = output_;
-  activation_output_.unaryExpr(activation_function_);
+  activation_output_ = activation_output_.unaryExpr(activation_function_);
 
   return activation_output_;
 }
 
-const Eigen::MatrixXd &Network::Backpropagate(const Eigen::MatrixXd &targets, double learning_rate) {
-  auto f_prime = [this](Eigen::MatrixXd in) {
-    in.unaryExpr(activation_function_derivative_);
-    return in;
-  };
+const Eigen::MatrixXd &Network::Backpropagate(const Eigen::MatrixXd &targets,
+                                              double learning_rate) {
 
-  output_errors_ = f_prime(activation_output_ - targets);// * f_prime(output_);
+  assert(targets.x() == activation_output_.x()
+         && targets.y() == activation_output_.y()
+         && "Targets and outputs need to be the same size");
+
+
+  output_errors_ = (BinaryCrossEntropyError(activation_output_, targets));
+
   const auto &nabla_b_ho = output_errors_;
   auto nabla_w_ho = output_errors_ * hidden_.transpose();
 
-  auto hidden_error = f_prime(hidden_output_weights_.transpose() * output_errors_);// * f_prime(hidden_);
+  // TODO(mateusz): Fix the stuff with the hardaman product
+  auto hidden_error = (hidden_output_weights_.transpose() * output_errors_)
+                          .unaryExpr((activation_function_derivative_));
+
   const auto &nabla_b_ih = hidden_error;
   auto nabla_w_ih = hidden_error * input_.transpose();
 
@@ -92,6 +97,5 @@ const Eigen::MatrixXd &Network::Backpropagate(const Eigen::MatrixXd &targets, do
 
   return output_errors_;
 }
-const Eigen::MatrixXd &Network::Error() {
-  return output_errors_;
-}
+
+const Eigen::MatrixXd &Network::Error() { return output_errors_; }
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,150 @@
 //
 // Created by mateusz on 11/22/2021.
 //
-#define PLOT
 
 #include "../include/Network.h"
 #include <chrono>
+#include <fstream>
 #include <numeric>
 
-#ifdef PLOT
+#ifdef MK_WITH_PLOT
 #include <sciplot/sciplot.hpp>
 #endif
 
 double random(double start, double stop) {
-  double x = (double) rand() / (double) RAND_MAX;
+  double x = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
   x /= (stop - start);
   x += start;
   return x;
 }
 
-int main() {
+struct LabelAndInput {
+  Eigen::MatrixXd label;
+  Eigen::MatrixXd input;
+};
+
+Eigen::MatrixXd ReadFile(std::fstream &fd) {
+
+  std::string broken_class_name;
+  int size_x, size_y;
+  fd >> broken_class_name;
+  fd >> size_x;
+  fd >> size_y;
+
+  Eigen::MatrixXd image(size_x, size_y);
+  for (size_t y = 0; y < size_y; y++) {
+    for (size_t x = 0; x < size_x; x++) {
+      char value;
+      fd >> value;
+      image(x, y) = value - '0';
+    }
+  }
+  return image;
+}
+
+class Dataset {
+ public:
+  Dataset(const std::string &path, size_t number_of_elements) {
+
+    Eigen::MatrixXd label_c(2, 1);
+    label_c(1) = 0;
+    label_c(0) = 1;
+
+    Eigen::MatrixXd label_o(2, 1);
+    label_o(1) = 1;
+    label_o(0) = 0;
+
+    for (int i = 0; i < number_of_elements; i++) {
+      std::fstream fdo, fdc;
+      auto name_o = path + "\\o" + std::to_string(i % 6) + ".txt";
+      auto name_c = path + "\\c" + std::to_string(i % 6) + ".txt";
+
+      fdo.open(name_o);
+      fdc.open(name_c);
+
+      if (fdc.bad() || fdo.bad())
+        assert(false && "file was bad");
+
+      dataset_.push_back(Distort({label_o, ReadFile(fdo)}, 0.01));
+      dataset_.push_back(Distort({label_c, ReadFile(fdc)}, 0.01));
+
+      fdo.close();
+      fdc.close();
+    }
+  }
+  const LabelAndInput &GetTrainingIteration(size_t i) {
+    return dataset_.at(i);
+  }
+
+  const LabelAndInput &GetTestingIteration(size_t i) {
+    return dataset_.at(i);
+  }
+
+  size_t Size() { return dataset_.size(); }
+
+  static LabelAndInput Distort(const LabelAndInput &data, double distortion_probabilty) {
+    //    return data;
+    auto copy = data;
+    for (size_t i = 0; i < copy.input.size(); i++) {
+      if (random(0, 1) <= distortion_probabilty) {
+        copy.input(i) = static_cast<Eigen::DenseCoeffsBase<class Eigen::Matrix<double, -1, -1, 0>, 1>::Scalar>(!static_cast<bool>(copy.input(i)));
+      }
+    }
+    return copy;
+  }
+
+ private:
+  std::vector<LabelAndInput> dataset_;
+};
+
+int images() {
+  Dataset d(R"(C:\w\neural-network-library\dataset)", 10000);
+  Network net(100, 10, 2);
+  std::vector<double> abs_err;
+  abs_err.reserve(d.Size());
+  std::vector<double> xs;
+  std::vector<double> predictions;
+  std::vector<double> labels;
+
+  for (int i = 0; i < d.Size(); i++) {
+    const auto &[label_raw, input_raw] = d.GetTrainingIteration(i);
+    auto input = input_raw.reshaped(100, 1);
+    auto label = label_raw.reshaped(2, 1);
+
+    auto out = net.FeedForward(input);
+    auto err = net.Backpropagate(label, 0.01);
+
+    abs_err.push_back(std::abs(err.sum()));
+    predictions.push_back(out(0));
+    labels.push_back(label(0));
+    xs.push_back(i);
+
+    std::cout << "Iter: " << i << " Err: " << abs_err.back() << std::endl;
+    std::cout << "Pred: " << out << " Label: " << label << std::endl;
+    if (i == 20) {
+      break;
+    }
+  }
+
+#ifdef MK_WITH_PLOT
+  sciplot::Plot plot;
+  plot.xlabel("Iteration");
+  plot.ylabel("Err");
+  //  plot.ytics().logscale();
+  plot.drawCurve(xs, abs_err).lineColor("blue").label("Error");
+  plot.show();
+  plot.clear();
+  plot.xlabel("Iteration");
+  //  plot.ylabel("Err");
+  //  plot.ytics().logscale();
+  plot.drawCurve(xs, labels).lineColor("blue").label("Label");
+  plot.drawCurve(xs, predictions).lineColor("red").label("Prediction");
+  plot.show();
+#endif
+  return 0;
+}
+
+int linear_function() {
   Network net(1, 4, 1);
 
   auto input = Eigen::MatrixXd(1, 1);
@@ -28,12 +154,17 @@ int main() {
   target.fill(2);
 
   auto f = [](double x) {
-    return 2 * x + 14;
+    return 2 * x;
   };
 
-  std::vector<double> xs, abs_err;
+  std::vector<double> xs;
+  std::vector<double> abs_err;
+
   std::vector<uint64_t> timings;
-  Eigen::MatrixXd out, err;
+
+  Eigen::MatrixXd out;
+  Eigen::MatrixXd err;
+
   int iterations = 100'000;
   timings.reserve(iterations);
   xs.reserve(iterations);
@@ -68,11 +199,11 @@ int main() {
   std::clog << std::endl
             << "Avg time (us): " << std::accumulate(timings.begin(), timings.end(), 0.0) / timings.size();
 
-#ifdef PLOT
+#ifdef MK_WITH_PLOT
   sciplot::Plot plot;
   plot.xlabel("Iteration");
   plot.ylabel("Err");
-//  plot.palette("dark2");
+  //  plot.palette("dark2");
   plot.ytics().logscale();
   plot.drawCurve(xs, abs_err).lineColor("blue").label("Error");
   plot.show();
@@ -80,4 +211,10 @@ int main() {
   plot.drawCurve(xs, timings).lineColor("red").label("Timings");
   plot.show();
 #endif
+  return 0;
+}
+
+int main() {
+  images();
+  //  linear_function();
 }
